85e64088fa6420c22e9080dc55a78c267e9b1046
sahel mastoureshgh <sahel.mastoureshgh@gmail.com>
2014-10-19 16:22:40 -0400

    Added picture and education part

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,27 +1,4 @@
-var bio = {
-	"name": "Sahel Mastoureshgh",
-	"role": "Software Engineer",
-	"contacts": {
-		"mobile": "323-895-0521",
-		"email": "sahel.mastoureshgh@gmail.com",
-		"github": "www.github.com/sahelmastoureshgh",
-		"address": "8815 Micheal Edward Dr, Louisville,KY,40291"
-	},
-	"welcomeMessage": "Welcome",
-	"skills": [
-		"programming", "web development", "JS"
-	],
-	"bioPic": "images/fry.jpg"
-}
-var projects = {
-	"projects": [{
-		"title": "News Website",
-		"dates": "November 2013",
-		"description": "Implemented a website which gathers the latest news feeds from xml format of different news agencies and has the ability to share news in Facebook. Using Node.js in server side running in Amazon EC2 cloud platform. For client side used Backbone.js, HTML5, Twitter bootstrap framework.",
-		"images": ["www.clicknewsup.com"]
 
-	}]
-}
 var work = {
 	"jobs": [{
 		"employer": "CBS Interactive",
@@ -52,21 +29,131 @@ var work = {
 
 	}]
 }
+
+//Display work json object in resume object
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formettedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formettedEmployerTitle);
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+		$(".work-entry:last").append(formattedDates);
+		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+		$(".work-entry:last").append(formattedDescription);
+
+	}
+
+}
+
+//Find all location that I have worked
+work.locationizer = function(_work) {
+	var allLocation = [];
+	for (var work in _work.jobs) {
+		var _workLoc = _work.jobs[work].location;
+		allLocation.push(_workLoc);
+	}
+	return allLocation;
+
+}
+//////////////////////////////////////////////////BIO/////////////////////////////////////////////////////////////////
+var bio = {
+	"name": "Sahel Mastoureshgh",
+	"role": "Software Engineer",
+	"contacts": {
+		"mobile": "323-895-0521",
+		"email": "sahel.mastoureshgh@gmail.com",
+		"github": "www.github.com/sahelmastoureshgh",
+		"location": "Louisville,KY"
+	},
+	"welcomeMessage": "Welcome to my resume website",
+	"skills": [
+		"programming", "web development", "JS","MongoDB","Java"
+	],
+	"bioPic": "images/sahelsmall.png"
+}
+
+//Dispay bio json object in resume page
+bio.display = function() {
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedMobile);
+	$("#footerContacts").append(formattedMobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").append(formattedEmail);
+	$("#footerContacts").append(formattedEmail);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	$("#topContacts").append(formattedGithub);
+	$("#footerContacts").append(formattedGithub);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+	$("#topContacts").append(formattedLocation);
+	$("#footerContacts").append(formattedLocation);
+	var formattedWeclome=HTMLWelcomeMsg.replace("%data%",bio.welcomeMessage);
+	$("#header").append(formattedWeclome);
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+	$("#header").append(formattedBioPic);
+	if (bio.skills.length > 0) {
+		$("#header").append(HTMLskillsStart);
+		for (var i = 0; i < bio.skills.length; i++) {
+			var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+			$("#skills").append(formattedSkill);
+		}
+	}
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+
+}
+
+//////////////////////////////////////////////////PROJET/////////////////////////////////////////////////////////////////
+
+var projects = {
+	"projects": [{
+		"title": "News Website",
+		"dates": "November 2013",
+		"description": "Implemented a website which gathers the latest news feeds from xml format of different news agencies and has the ability to share news in Facebook. Using Node.js in server side running in Amazon EC2 cloud platform. For client side used Backbone.js, HTML5, Twitter bootstrap framework.",
+		"images": ["www.clicknewsup.com"]
+
+	}]
+}
+//Display projects in resume page
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project].images) {
+				var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImages);
+			}
+		}
+
+	}
+
+}
+
+//////////////////////////////////////////////////EDUCATION/////////////////////////////////////////////////////////////////
 var education = {
 	"schools": [{
 		"name": "Universtity Of Tehran",
 		"location": "Tehran, Iran",
 		"degree": "Bachelor",
 		"majors": ["ComputerScience"],
-		"dates": "August 2006 - May 2009",
-		"url": "www.google.com"
+		"dates": "August 2006 - May 2009"
+
 	}, {
 		"name": "Worcester Polytechnic Institute",
 		"location": "Worcester, MA",
 		"degree": "Master",
 		"majors": ["ComputerScience"],
-		"dates": "August 2010 - May 2012",
-		"url": "www.wpi.edu"
+		"dates": "August 2010 - May 2012"
+
 	}],
 	"onlineCourses": [{
 		"title": "Tackling the Challenges of Big Data",
@@ -86,77 +173,43 @@ var education = {
 		"url": "www.wpi.edu"
 	}]
 }
-
-
-
-/**
-Display work json object in resume object
-**/
-
-work.display=function() {
-	for (job in work.jobs) {
-		$("#workExperience").append(HTMLworkStart);
-		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
-		var formettedEmployerTitle = formattedEmployer + formattedTitle;
-		$(".work-entry:last").append(formettedEmployerTitle);
-		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
-		$(".work-entry:last").append(formattedDates);
-		var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
-		$(".work-entry:last").append(formattedDescription);
-
-	}
-
-}
-
-//Find all location that I have worked
-function locationizer(_work) {
-    var allLocation=[];
-    for (var work in _work.jobs){
-        var _workLoc=_work.jobs[work].location;
-        allLocation.push(_workLoc);
-    }
-    return allLocation;
-
-}
-
-//Dispay bio json object in resume page
-bio.display=function(){
-	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-	if (bio.skills.length > 0) {
-		$("#header").append(HTMLskillsStart);
-		for (var i = 0; i < bio.skills.length; i++) {
-			var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-			$("#skills").append(formattedSkill);
-		}
-	}
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-	
-}
-//Display projects in resume page
-projects.display=function(){
-	for (project in projects.projects) {
-		$("#projects").append(HTMLprojectStart);
-		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
-		$(".project-entry:last").append(formattedTitle);
-		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
-		$(".project-entry:last").append(formattedDates);
-		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
-		$(".project-entry:last").append(formattedDescription);
-		if(projects.projects[project].images.length>0){
-			for (image in projects.projects[project].images){
-				var formattedImages = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-				$(".project-entry:last").append(formattedImages);
+//Display education in my resume page
+education.display = function() {
+	for (edu in education.schools) {
+		$('#education').append(HTMLschoolStart);
+		var formattedName = HTMLschoolName.replace("%data%", education.schools[edu].name);
+		var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[edu].degree);
+		var formattedNameAndDegree=formattedName+formattedDegree;
+		$(".education-entry:last").append(formattedNameAndDegree);
+		var formattedDate = HTMLschoolDates.replace("%data%", education.schools[edu].dates);
+		$(".education-entry:last").append(formattedDate);
+		var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[edu].location);
+		$(".education-entry:last").append(formattedLocation);
+		if (education.schools[edu].majors.length > 0) {
+			for (major in education.schools[edu].majors) {
+				var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[edu].majors[major]);
+				$(".education-entry:last").append(formattedMajor);
 			}
 		}
+	
+	}
+	//online classes
+	$('#education').append(HTMLonlineClasses)
+	for (online in education.onlineCourses) {
+		$('#education').append(HTMLschoolStart);
+		var formattedTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[online].title);
+		var formattedSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[online].school);
+		var formattedTitleAndSchool=formattedTitle+formattedSchool;
+		$(".education-entry:last").append(formattedTitleAndSchool);
+		var formattedDate = HTMLonlineDates.replace("%data%", education.onlineCourses[online].dates);
+		$(".education-entry:last").append(formattedDate);
+		var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[online].url);
+		$(".education-entry:last").append(formattedURL);
 
 	}
-	$("#projects").append(HTMLprojectStart);
-	
+
 }
-//Display education in my resume page
 work.display();
 bio.display();
 projects.display();
+education.display();
